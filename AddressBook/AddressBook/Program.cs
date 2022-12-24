6c2d0b7d10c834aff7e32c33ec1b53c11585f552
qismat <qismatnisha6243@gmail.com>
2022-12-24 23:51:30 +0530

    [qismat] Add : UC3-EditContactDetails

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -2,10 +2,8 @@
 
 namespace AddressBook
 {
-    internal class Program
+    public class Program
     {
-        
-
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
@@ -16,7 +14,8 @@ namespace AddressBook
             {
                 Console.WriteLine("Please select the Options :\n" +
                     "1)Enter Details to add Contact in List\n" +
-                    "2)Add new Contact, Display and Stores contact in the list\n");
+                    "2)Add new Contact, Display and Stores contact in the list\n" +
+                    "3)Edit Details of Contact\n");
 
                 int option = Convert.ToInt32(Console.ReadLine());
 
@@ -40,12 +39,51 @@ namespace AddressBook
                         addobj1.addPerson();
                         break;
 
+                    case 3:
+
+                        AddressBook addobj2 = new AddressBook();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+
+                        bool check1 = true;
+
+                        while (check1)
+                        {
+                            Console.WriteLine("Follow Steps to add details:\n" +
+                               "1) To addContact\n" + "2) To Edit Contact\n" +
+                               "3) To Display Contact\n");
+
+                            int select = Convert.ToInt32(Console.ReadLine());
+                            switch (select)
+                            {
+                                case 1:
+                                    addobj2.addPerson();
+                                    Console.WriteLine(":::::::::::::::::::::::::::;");
+                                    break;
+
+                                case 2:
+                                    addobj2.EditContact();
+                                    Console.WriteLine(":::::::::::::::::::::::::::;");
+                                    break;
+
+                                case 3:
+                                    addobj2.Display();
+                                    Console.WriteLine(":::::::::::::::::::::::::::;");
+                                    break;
+
+                                default:
+
+                                    check = !check;
+                                    break;
+                            }
+                        }
+                        break;
 
                     default:
                         Console.WriteLine("Please select proper option");
                         break;
                 }
                 check = false;
+                Console.WriteLine("Try Again ");
                 break;
             }
         }

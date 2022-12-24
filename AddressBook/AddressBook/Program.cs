337406a7d0386c3cf5db4cf7f061c561914d23be
qismat <qismatnisha6243@gmail.com>
2022-12-24 20:18:26 +0530

    [qismat] Add : UC1-ContactDetails

---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact contact = new Contact()
+            {
+                FirstName = "Qismat ",
+                LastName = "Khan",
+                Address = "Lucknow",
+                City = "Sultanpur",
+                State = "Uttar Pradesh",
+                Zip = 28001,
+                PhoneNumber = 8971919599,
+                Email = "qismatnisha6243@gmail.com",
+            };
+            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+        }
+    }
+}

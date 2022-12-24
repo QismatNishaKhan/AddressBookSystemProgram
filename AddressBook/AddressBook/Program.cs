bbe86fe7cdc1a1f36069b55698c24fa285592fe5
qismat <qismatnisha6243@gmail.com>
2022-12-24 23:28:04 +0530

    [qismat] Add : UC2-AddNewContact

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -1,21 +1,53 @@
-﻿namespace AddressBook
+﻿using Windows.ApplicationModel.Contacts;
+
+namespace AddressBook
 {
     internal class Program
     {
-        static void Main(string[] args)
+        
+
+        public static void Main(string[] args)
         {
-            Contact contact = new Contact()
+            Console.WriteLine("Welcome to Address Book Program");
+
+            bool check = true;
+
+            while (check)
             {
-                FirstName = "Qismat ",
-                LastName = "Khan",
-                Address = "Lucknow",
-                City = "Sultanpur",
-                State = "Uttar Pradesh",
-                Zip = 28001,
-                PhoneNumber = 8971919599,
-                Email = "qismatnisha6243@gmail.com",
-            };
-            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.PhoneNumber + "\n" + contact.Email);
+                Console.WriteLine("Please select the Options :\n" +
+                    "1)Enter Details to add Contact in List\n" +
+                    "2)Add new Contact, Display and Stores contact in the list\n");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+
+                        AddressBook addobj = new AddressBook();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+                        addobj.createContact();
+                        break;
+
+                    case 2:
+
+                        AddressBook addobj1 = new AddressBook();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+                        addobj1.createContact();
+                        Console.WriteLine("::::::::::::::::::::::::::");
+                        Console.WriteLine("Entered Details of Person given as :");
+                        addobj1.Display();
+                        addobj1.addPerson();
+                        break;
+
+
+                    default:
+                        Console.WriteLine("Please select proper option");
+                        break;
+                }
+                check = false;
+                break;
+            }
         }
     }
 }

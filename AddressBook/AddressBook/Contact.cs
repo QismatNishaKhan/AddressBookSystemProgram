f94ea691c3b5ab8cfe8a5a57597074a7efc3453a
qismat <qismatnisha6243@gmail.com>
2023-01-15 18:00:59 +0530

    [qismat] Add : UC7-PreventDuplicateContact

--- AddressBook/AddressBook/Contact.cs
+++ AddressBook/AddressBook/Contact.cs
@@ -10,14 +10,15 @@ namespace AddressBook
     public class Contact
     {
 
+
         //Using get and set method to access 
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public int Zip { get; set; }
-        public long PhoneNo { get; set; }
-        public string Email { get; set; }
+        public string firstName;
+        public string lastName;
+        public string address;
+        public int phone;
+        public string city;
+        public string state;
+        public int zipcode;
+        public string email;
     }
 }
